61f02f6c8f8c8e91fa79dc82ff17677198d16e34
Omar Dachia <omardachia@gmail.com>
2024-04-03 14:51:02 +0100

    Eight don

---
+++ Q8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+  cout << "#7:-> Write a program to find circumference of a circle."<<endl;
+  cout << "\n======================================================";
+  cout << "\nProgram Start here:->" << endl;
+  cout << "\n+++++++++++++++++++++"<<endl;
+
+  double r = 0;
+  cout << "Type Radiue: "<<endl;
+  cin >> r;
+
+  cout << "circumference ="<< (2*(22/7)*r) <<endl;
+
+
+  return 0;
+}
+
+
+
+
+
